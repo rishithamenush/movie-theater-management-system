63f54e296112fc9082a12cee0ff008f6dad6f36e
rishitha.menush <rishitha.menush@gmail.com>
2024-03-03 22:24:37 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,18 +1,13 @@
 <?php
 include('config.php');
 session_start();
-date_default_timezone_set('Asia/Kolkata');
+date_default_timezone_set('Asia/Colombo');
 ?>
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
+
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Free Theater Website Template | About :: w3layouts</title>
+<title>Cineplex Movie Theatre</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
